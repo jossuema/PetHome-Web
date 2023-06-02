0dd81b196e0fef01a81be0c42314028c2d7b6eb3
jossuema <negritomallapqs@hotmail.com>
2023-06-01 20:52:46 -0500

    Initial repo

---
+++ src/main/java/com/mascotasfelices/demo/PethomeApplication.java
@@ -0,0 +1,13 @@
+package com.mascotasfelices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PethomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PethomeApplication.class, args);
+    }
+
+}
